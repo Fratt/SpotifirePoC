4b4bbd761f966d97c7d2f82900f83c339bcca7b3
Christophe Frattino <christophe@frattino.ch>
2019-04-27 11:13:52 +0200

    PoC search for track

--- src/main/java/ch/frattino/spotifirepoc/SpotifirePocApplication.java
+++ src/main/java/ch/frattino/spotifirepoc/SpotifirePocApplication.java
@@ -1,13 +1,22 @@
 package ch.frattino.spotifirepoc;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpotifirePocApplication {
+public class SpotifirePocApplication implements CommandLineRunner {
+
+	private static Logger LOG = LoggerFactory.getLogger(SpotifirePocApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpotifirePocApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) {
+		LOG.info("Run the various unit tests.");
+	}
 }
--- src/test/java/ch/frattino/spotifirepoc/SpotifirePocApplicationTests.java
+++ src/test/java/ch/frattino/spotifirepoc/SpotifirePocApplicationTests.java
@@ -1,16 +1,91 @@
 package ch.frattino.spotifirepoc;
 
+import com.wrapper.spotify.SpotifyApi;
+import com.wrapper.spotify.exceptions.SpotifyWebApiException;
+import com.wrapper.spotify.model_objects.credentials.ClientCredentials;
+import com.wrapper.spotify.model_objects.specification.Paging;
+import com.wrapper.spotify.model_objects.specification.Track;
+import com.wrapper.spotify.requests.authorization.client_credentials.ClientCredentialsRequest;
+import com.wrapper.spotify.requests.data.search.simplified.SearchTracksRequest;
+import org.apache.juneau.json.JsonSerializer;
+import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpotifirePocApplicationTests {
 
+	private static Logger LOG = LoggerFactory.getLogger(SpotifirePocApplicationTests.class);
+
+	private static SpotifyApi spotifyApi;
+
+	@BeforeClass
+	public static void setup() throws URISyntaxException {
+
+		String clientId = readEnvOrFail("SPOTIFY_CLIENT_ID");
+		String clientSecret = readEnvOrFail("SPOTIFY_CLIENT_SECRET");
+
+		// Wet set up the API
+		spotifyApi = new SpotifyApi.Builder()
+				.setClientId(clientId)
+				.setClientSecret(clientSecret)
+				.setAccessToken("tagol")
+				.setRedirectUri(new URI("http://localhost"))
+				.build();
+
+		// We grab a "guest access token"
+		ClientCredentialsRequest clientCredentialsRequest = spotifyApi.clientCredentials().build();
+		ClientCredentials clientCredentials = null;
+		try {
+			clientCredentials = clientCredentialsRequest.execute();
+		} catch (IOException | SpotifyWebApiException e) {
+			LOG.error("Couldn't get a guest access token", e);
+			Assert.fail("Couldn't get a guest access token");
+			return;
+		}
+		spotifyApi.setAccessToken(clientCredentials.getAccessToken());
+	}
+
+	private static String readEnvOrFail(String key) {
+		String value = System.getenv(key);
+		if (value == null) {
+			Assert.fail("You forgot to set the " + key + " environment variable!");
+		}
+		return value;
+	}
+
 	@Test
-	public void contextLoads() {
+	public void searchForTrack() {
+		SearchTracksRequest request = spotifyApi.searchTracks("Lost in the Twilight Hall").build();
+		try {
+			Paging<Track> paging = request.execute();
+			JsonSerializer jsonSerializer = JsonSerializer.DEFAULT_READABLE;
+			for (Track track : paging.getItems()) {
+				LOG.info(jsonSerializer.serialize(track));
+			}
+		} catch (Exception e) {
+			LOG.error("Error", e);
+		}
+	}
+
+	@Test
+	public void searchForAlbum() {
+		// TODO
+	}
+
+	@Test
+	public void createPlaylist() {
+		// TODO
 	}
 
 }
