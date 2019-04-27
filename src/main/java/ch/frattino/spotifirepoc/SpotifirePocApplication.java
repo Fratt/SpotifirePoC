aa723c69ea891ce00c8f2d369c413baf83b7e476
Christophe Frattino <christophe@frattino.ch>
2019-04-27 10:33:54 +0200

    Initial commit

---
+++ src/main/java/ch/frattino/spotifirepoc/SpotifirePocApplication.java
@@ -0,0 +1,13 @@
+package ch.frattino.spotifirepoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifirePocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifirePocApplication.class, args);
+	}
+
+}
